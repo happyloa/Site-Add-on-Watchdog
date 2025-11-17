e99aa1d84f66eebc1f244fdacf62adb9baf9bcff
Aaron <83556435+happyloa@users.noreply.github.com>
2025-11-17 17:10:20 +0800

    Shorten cron disabled notice string

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use function Brain\Monkey\Functions\expect;
 use function Brain\Monkey\Functions\when;
 use Watchdog\Notifier;
 use Watchdog\Plugin;
@@ -9,6 +10,80 @@ use Watchdog\Scanner;
 
 class PluginTest extends TestCase
 {
+    public function testScheduleTriggersOverdueCatchUpForTesting(): void
+    {
+        when('site_url')->justReturn('https://example.test');
+        when('wp_remote_post')->justReturn(null);
+        when('get_option')->justReturn([
+            'overdue_streak' => 0,
+            'cron_disabled'  => false,
+        ]);
+        when('is_admin')->justReturn(false);
+        when('current_user_can')->justReturn(false);
+        when('update_option')->justReturn(true);
+
+        expect('wp_next_scheduled')
+            ->once()
+            ->with('wp_watchdog_scheduled_scan')
+            ->andReturn(time() - 2_000);
+        expect('wp_get_schedule')->once()->andReturn('testing');
+        expect('wp_get_schedules')->once()->andReturn([
+            'testing' => ['interval' => 600],
+        ]);
+        expect('spawn_cron')->once();
+        expect('wp_schedule_single_event')->once();
+
+        $scanner = $this->createMock(Scanner::class);
+        $riskRepository = $this->createMock(RiskRepository::class);
+        $settingsRepository = $this->createMock(SettingsRepository::class);
+        $settingsRepository->method('get')->willReturn([
+            'notifications' => ['frequency' => 'testing'],
+        ]);
+        $notifier = $this->createMock(Notifier::class);
+
+        $plugin = new Plugin($scanner, $riskRepository, $settingsRepository, $notifier);
+        $plugin->schedule();
+    }
+
+    public function testScheduleWarnsWhenCronIsDisabled(): void
+    {
+        if (! defined('DISABLE_WP_CRON')) {
+            define('DISABLE_WP_CRON', true);
+        }
+
+        when('site_url')->justReturn('https://example.test');
+        when('wp_remote_post')->justReturn(null);
+        when('get_option')->justReturn([
+            'overdue_streak' => 1,
+            'cron_disabled'  => true,
+        ]);
+        when('is_admin')->justReturn(false);
+        when('current_user_can')->justReturn(false);
+        when('update_option')->justReturn(true);
+
+        expect('wp_next_scheduled')
+            ->once()
+            ->with('wp_watchdog_scheduled_scan')
+            ->andReturn(time() - 3_000);
+        expect('wp_get_schedule')->once()->andReturn('testing');
+        expect('wp_get_schedules')->once()->andReturn([
+            'testing' => ['interval' => 600],
+        ]);
+        expect('spawn_cron')->never();
+        expect('wp_schedule_single_event')->once();
+
+        $scanner = $this->createMock(Scanner::class);
+        $riskRepository = $this->createMock(RiskRepository::class);
+        $settingsRepository = $this->createMock(SettingsRepository::class);
+        $settingsRepository->method('get')->willReturn([
+            'notifications' => ['frequency' => 'testing'],
+        ]);
+        $notifier = $this->createMock(Notifier::class);
+
+        $plugin = new Plugin($scanner, $riskRepository, $settingsRepository, $notifier);
+        $plugin->schedule();
+    }
+
     public function testTestingFrequencyAlwaysNotifies(): void
     {
         $scanner = $this->createMock(Scanner::class);
